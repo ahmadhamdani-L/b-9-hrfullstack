e7aa824033db791af69737b7ee4459a7ab90bea5
kangdiancp <kangdiancp@gmail.com>
2021-04-20 13:12:38 +0700

    day01 done

--- server/controllers/RegionCtrl.js
+++ server/controllers/RegionCtrl.js
@@ -1,10 +1,61 @@
+import { sequelize } from '../../config/config-db';
 
 // findAll = select * from regions
 const findAll = async (req, res) => {
     const regions = await req.context.models.Regions.findAll();
     return res.send(regions);
 }
+// findone = select * from regions where region_id=:id
+const findOne = async (req, res) => {
+    const regions = await req.context.models.Regions.findOne({
+        where: { region_id: req.params.id }
+    });
+    return res.send(regions);
+}
+
+// create new region
+const create = async (req, res) => {
+    const regions = await req.context.models.Regions.create({
+        region_name: req.body.region_name
+    });
+    return res.send(regions);
+}
+
+// update regions set region_name=:2,region_desc=:4 where region_id=:3
+const update = async (req, res) => {
+    const { region_name } = req.body;
+    const regions = await req.context.models.Regions.update(
+        { region_name: region_name },// nama attribute yg akan di update
+        { returning: true, where: { region_id: req.params.id } }
+    );
+    return res.send(regions);
+}
+
+// delete 
+const remove = async (req, res) => {
+    await req.context.models.Regions.destroy({
+          where: { region_id: req.params.id }
+    }).then(result =>{
+        console.log(result);
+        return res.send("delete "+result+" rows.");
+    });
+    
+}
+
+const rawSQL = async (req, res) => {
+    await sequelize.query('SELECT * FROM regions where region_id = :regionId',
+        { replacements: { regionId: parseInt(req.params.id) }, type: sequelize.QueryTypes.SELECT } 
+    ).then(result => {
+        return res.send(result);
+    })
+}
+
 
 export default {
-    findAll
+    findAll,
+    findOne,
+    create,
+    update,
+    remove,
+    rawSQL
 }
--- server/routes/RegionRoute.js
+++ server/routes/RegionRoute.js
@@ -2,6 +2,11 @@ import { Router } from 'express';
 import IndexCtrl from '../controllers/IndexController'
 
 const router = Router();
+router.post('/', IndexCtrl.RegionCtrl.create);
 router.get('/', IndexCtrl.RegionCtrl.findAll);
+router.get('/:id', IndexCtrl.RegionCtrl.findOne);
+router.put('/:id', IndexCtrl.RegionCtrl.update);
+router.delete('/:id', IndexCtrl.RegionCtrl.remove);
+router.get('/rawsql/:id', IndexCtrl.RegionCtrl.rawSQL);
 
 export default router;
