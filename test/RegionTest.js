89a9125aafdbf0987fa1ca338beb36a09bb04484
kangdiancp <kangdiancp@gmail.com>
2021-04-21 10:18:10 +0700

    add unit testing

---
+++ test/RegionTest.js
@@ -0,0 +1,41 @@
+import server from '../server/server'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+
+// Assertion 
+chai.should();
+chai.use(chaiHttp);
+
+describe('region APIs', () => {
+    // get all regions
+    describe("Test GET route /api/regions", () => {
+
+        it("It should return all regions", (done) => {
+           
+            chai.request(server)
+                .get("/api/regions/")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                    response.body.length.should.not.be.eq(0);
+                    done();
+                });
+
+        });
+
+        // negative test
+        it("It should not return all regions", (done) => {
+            chai.request(server)
+                .get("/api/regio/")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                    done();
+                });
+        });
+
+    });
+
+    // end get all regions
+
+    
+});
